f5f687ce447c9cf23b57f558d3a8a542df3a2977
Julius Radl <julius.radl@student.hswt.de>
2026-05-01 11:06:29 +0200

    default app aufruf ausprobiert

--- src/main/java/com/radl/sa/fastareader/FastaController.java
+++ src/main/java/com/radl/sa/fastareader/FastaController.java
@@ -1,7 +1,11 @@
 package com.radl.sa.fastareader;
 
+import java.awt.Desktop;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Map;
 
 import javax.swing.SwingWorker;
 
@@ -80,6 +84,39 @@ public class FastaController implements FastaControllable {
 	
 	public void pressedBrowserButton() {
 		fv.openBrowser();
+
+		// Flexibel
+		if (!Desktop.isDesktopSupported()) { return; }
+		try {
+			Desktop.getDesktop().browse(new URI("http://www.ncbi.nlm.nih.gov/Genbank/"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// wd des Prozesses anpassen
+		// pb.directory(new File("~/");
+		// Angehängte Environmental Variables anzeigen
+		
+		
+		// Windows only Lösung mit Process Builder
+//		ProcessBuilder pb = new ProcessBuilder(
+//				"rundll32",
+//				"url.dll,FileProtocolHandler",
+//				"http://www.ncbi.nlm.nih.gov/Genbank/");
+//		Map<String, String> env = pb.environment();
+//		env.forEach( (String key, String value) -> System.out.println(key + " = " + value));
+//		try {
+//			Process p = pb.start();
+//			// kann Prozess beenden, aber der Default Browser wird sich 
+//			// dadurch nicht schließen
+//			p.destroy();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 	
 	public void setView(FastaView fv) {
